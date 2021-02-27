0c9081bf1457af33e8ff2ca195e5d1fce2a99622
zhangxu409 <zhangxu409@gmail.com>
2021-02-27 21:24:45 +0100

    optimise countdown loading time

--- script.js
+++ script.js
@@ -35,6 +35,19 @@ x.addEventListener("click", changeHeartPosition)
 // Set the date we're counting down to
 var countDownDate = new Date("Jun 5, 2021 12:00:00").getTime();
 
+// Set default count down
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+
+
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
